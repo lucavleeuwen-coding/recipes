829a3350d53299466742522c3eda284b63ab725c
lucavanleeuwen <lucavleeuwen@outlook.com>
2021-11-15 10:12:19 +0100

    email functie toegevoegd. En mapping door relatie ingredienten.

---
+++ src/Controller/LoginController 2.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function index(AuthenticationUtils $authenticationUtils): \Symfony\Component\HttpFoundation\Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+}
